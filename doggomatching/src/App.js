a7ee285bcc99e2968da8dc1fe25cebbe57ceb8c5
AmandaJisoo <amandapark8988@gmail.com>
2020-12-21 21:32:43 -0800

    now says home

--- doggomatching/src/App.js
+++ doggomatching/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      </BrowserRouter>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
