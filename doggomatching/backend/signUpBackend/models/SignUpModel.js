6e940959cb4de1de9aafb6878733fee4cfa63212
AmandaJisoo <amandapark8988@gmail.com>
2020-12-24 01:16:38 -0800

    schema initally added

---
+++ doggomatching/backend/signUpBackend/models/SignUpModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const signUpTemplate = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: Number,
+        required: true
+    },
+    nameOfPet: {
+        type: String,
+        required: true
+    },
+    sizeOfPet: {
+        type: String,
+        required: true
+    },
+    sexOfPet: {
+        type: String,
+        required: true
+    },
+    ageOfPet: {
+        type: Number,
+        required: true
+    },
+    personalityOfPet:{
+        type: String,
+        required: true
+    },
+    sizePrefereceToBeFrined:{
+        type: [String],
+        required: true
+    },
+    areaOfInterest: {
+        type:String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('mytable', signUpTemplate)
