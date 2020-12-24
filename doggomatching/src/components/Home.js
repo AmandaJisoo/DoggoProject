91fc0ff50cadd905f37cd5cc7b5a34e5c1c99f77
AmandaJisoo <amandapark8988@gmail.com>
2020-12-24 02:35:28 -0800

    home page added but need to add tags and sign up btn

--- doggomatching/src/components/Home.js
+++ doggomatching/src/components/Home.js
@@ -1,12 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import homePic from '../resource/homePic.jpg';
 
 const Home = () => {
     return (
-        <main>
-            <div>
-                Hello
+        <div>
+            {/* <div className="container"> */}
+                <header className="container" id="pic">
+                    <h1>Welcome to my LandingPage</h1>
+                    <p>How are you doing?</p>
+                    <span id="down">&#8595;</span>
+                    <button className="btn">Button</button>
+                </header>
+    
+            {/* </div> */}
+            <div className="main">
+                <h3 style={{ "text-align": "center" }}>Some Sample Content</h3>
+                <p>
+                    Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+                </p>
+                <p>
+                    Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+                </p>
+                <p>
+                    Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+                </p>
+                <p>
+                    Web browsers receive HTML documents from a web server or from local storage and render the documents into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+                </p>
             </div>
-        </main>
+        </div>
     )
 }
 
